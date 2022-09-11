c606c6566752bbc28ff337fed7790d77fe5cd579
imnikhilanand <imnikhilanand@gmail.com>
2022-09-11 02:58:19 -0400

    Added libraries to connect kafka, cassandra and spark

--- src/consume_data/consumer.scala
+++ src/consume_data/consumer.scala
@@ -3,3 +3,15 @@
 bin/spark-shell --packages "com.datastax.spark:spark-cassandra-connector_2.12:3.0.0","org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.1"
 
 */
+
+// Importing the libraries
+import org.apache.spark.streaming._
+import org.apache.spark.SparkContext
+import com.datastax.spark.connector.SomeColumns
+import com.datastax.spark.connector.cql.CassandraConnector
+import com.datastax.spark.connector.streaming._
+import org.apache.spark.streaming.kafka010._
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.TopicPartition
+
+
