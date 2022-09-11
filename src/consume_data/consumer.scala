ca2fd2d56d109d632b53901a3999adf1e2963ab1
imnikhilanand <imnikhilanand@gmail.com>
2022-09-11 03:03:02 -0400

    Added code to listen to kafka topic

--- src/consume_data/consumer.scala
+++ src/consume_data/consumer.scala
@@ -14,4 +14,16 @@ import org.apache.spark.streaming.kafka010._
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.TopicPartition
 
+// creating sparkcontext
+val sc = SparkContext.getOrCreate
+
+// reading streaming data at an interval of 10 seconds
+val ssc = new StreamingContext(sc, Seconds(10))
+
+val preferredHosts = LocationStrategies.PreferConsistent
+
+// listening to topic registered_user_2 (topic name)
+val topics = List("registered_user_2")
+
+
 
