7ccca1954198a5beba5a1d903529e40311265908
imnikhilanand <imnikhilanand@gmail.com>
2022-09-11 02:52:35 -0400

    Added the scala consumer file

---
+++ src/consume_data/consumer.scala
@@ -0,0 +1,5 @@
+/* Run this in code in terminal to download libraries to connect kafka with spark and cassandra with spark (must be done every time one starts the program)
+
+bin/spark-shell --packages "com.datastax.spark:spark-cassandra-connector_2.12:3.0.0","org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.1"
+
+*/
